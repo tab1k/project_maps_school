ba41ff2050a6d2669c70d36c426a9205d194f89f
tab1k <tabigatkarbayev@icloud.com>
2023-02-05 02:00:29 +0600

    republic

--- main/admin.py
+++ main/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'description', 'image'] # показ
--- subjects/views.py
+++ subjects/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import *
+# CBV - CLASS BASED VIEWS (ListView)
 
-# Create your views here.
+
+
+class Python(ListView):
+    model = Subject
+    template_name = 'subjects/python.html'
+    categories = model.objects.all()
+    for category in categories:
+        print(category.name)
+
+
+
+class C_plus(ListView):
+    model = Subject
+    template_name = 'subjects/c_plus.html'
+
+class Computer_science(ListView):
+    model = Subject
+    template_name = 'subjects/computer_science.html'
+
+
+class English(ListView):
+    model = Subject
+    template_name = 'subjects/english.html'
+
+class Math(ListView):
+    model = Subject
+    template_name = 'subjects/math.html'
+
+class Web_design(ListView):
+    model = Subject
+    template_name = 'subjects/web_design.html'
